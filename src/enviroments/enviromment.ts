d7b0e88cfb1934d8714f98d86c11728d8f64b953
James <j.r.precilla365@gmail.com>
2025-02-07 22:36:28 +0100

    change of backend api

--- src/enviroments/enviromment.ts
+++ src/enviroments/enviromment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   /*apiUrl: 'http://localhost:3000'*/ 
-  apiUrl: 'https://nikeproject-dm78.onrender.com'  
+  /*apiUrl: 'https://nikeproject-dm78.onrender.com'  */
+  apiUrl: 'https://nikeproject-production.up.railway.app'
 };
