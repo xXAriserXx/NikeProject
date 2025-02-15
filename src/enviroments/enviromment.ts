050daf0cd443326480fb422539779319e5ad8eee
James <j.r.precilla365@gmail.com>
2025-02-15 08:26:01 +0100

    used wrong apiurl

--- src/enviroments/enviromment.ts
+++ src/enviroments/enviromment.ts
@@ -1,6 +1,6 @@
 export const environment = {
  /* apiUrl: 'http://localhost:3000'*/
- 
-apiUrl: 'https://nikeproject-dm78.onrender.com'
+
+apiUrl: 'https://nikeproject-rela.onrender.com'
 /*apiUrl: 'https://nikeproject-production.up.railway.app'*/
 };
